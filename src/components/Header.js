a464c87b1f4142d69c1ff5775b2b998cca65ad97
nurgnc <gencbedriyenur@gmail.com>
2021-04-01 13:37:12 +0300

    created header and swiper slider

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,31 @@
 import React from 'react'
+import SwiperCore, { Navigation, Pagination, A11y } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { SliderImages } from './Items/SliderImages'
+import './Header.css'
+import 'swiper/swiper.min.css';
+import 'swiper/components/navigation/navigation.min.css';
+import 'swiper/components/pagination/pagination.min.css';
+
+SwiperCore.use([Navigation, Pagination, A11y]);
 
 const Header = () => {
     return (
-        <div>
-            Header
-        </div>
+        <Swiper
+            className="container swiper-slider"
+            navigation
+            pagination={{ clickable: true }}
+            spaceBetween={50}
+            slidesPerView={3}
+        >
+        {SliderImages.map((slide, index) => {
+            return ( 
+                <SwiperSlide className="slider-images">
+                    <img key={index} src={slide.image} alt="Slider Image" />
+                </SwiperSlide>
+            )
+        })}
+        </Swiper>
     )
 }
 
---
+++ src/components/Items/SliderImages.js
@@ -0,0 +1,14 @@
+export const SliderImages = [
+    {
+        image: 'https://via.placeholder.com/591x502.png/'
+    },
+    {
+        image: 'https://via.placeholder.com/292x502.png/'
+    },
+    {
+        image: 'https://via.placeholder.com/292x502.png/'
+    },
+    {
+        image: 'https://via.placeholder.com/292x502.png/'
+    }
+]
